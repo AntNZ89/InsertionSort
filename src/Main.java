42e5e47ede84c728a14d8ce1e58dc3032ad9d12f
antND <antonlenz508@gmail.com>
2025-08-24 15:26:14 +0200

    Update

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,22 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
-        sortArray();
-
-    }
-
-    public static void sortArray(){
-
-        int[] array = new int[10];
-        int temp = 0;
         Random random = new Random();
+        int[] array = new int[10];
 
         for (int c = 0 ; c < array.length ; c++){
             array[c] = random.nextInt(0, 100);
         }
+
+        sortArray(array);
+
+
+
+    }
+
+    public static void sortArray(int[] array){
+
+        int temp = 0;
 
         for (int c = 1 ; c < array.length ; c++){
             for (int r = c ; r > 0 ; r-- ){
